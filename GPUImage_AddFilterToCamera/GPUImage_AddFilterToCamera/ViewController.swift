e887a5bd14787804f62a6ad20b10660a79bfe9dd
Mazy <mazongyong@163.com>
2017-04-22 13:23:43 +0800

    初始化项目1：利用GPUImage给相机加滤镜

---
+++ GPUImage_AddFilterToCamera/GPUImage_AddFilterToCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPUImage_AddFilterToCamera
+//
+//  Created by  Mazy on 2017/4/22.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
