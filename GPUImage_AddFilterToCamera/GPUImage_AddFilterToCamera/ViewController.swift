01ca89bb1533be1ec62ea61fd76364e1f7c0b808
Mazy <mazongyong@163.com>
2017-04-22 14:46:22 +0800

    feat：完成美颜滤镜相机

--- GPUImage_AddFilterToCamera/GPUImage_AddFilterToCamera/ViewController.swift
+++ GPUImage_AddFilterToCamera/GPUImage_AddFilterToCamera/ViewController.swift
@@ -24,11 +24,78 @@
 import UIKit
 import GPUImage
 
+let kWidth = UIScreen.main.bounds.size.width
+let kHeight = UIScreen.main.bounds.size.height
+
 class ViewController: UIViewController {
 
+    // 步骤一：初始化相机，第一个参数表示相册的尺寸，第二个参数表示前后摄像头
+    let camera: GPUImageStillCamera = GPUImageStillCamera(sessionPreset: AVCaptureSessionPreset1280x720, cameraPosition: AVCaptureDevicePosition.front)
+    
+    let imageView = GPUImageView(frame: CGRect(origin: CGPoint.zero, size: UIScreen.main.bounds.size))
+    
+    
+    var filtersArray: [AnyObject] = [AnyObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.insertSubview(imageView, at: 0)
+        
+        // 设置相机方向为竖屏
+        camera.outputImageOrientation = .portrait
+        
+        let groupFilter = GPUImageFilterGroup()
+        
+        // 磨皮滤镜
+        let bilateralFilter = GPUImageBilateralFilter()
+            groupFilter.addTarget(bilateralFilter)
+        
+        // 美白滤镜
+        let brightnessFilter = GPUImageBrightnessFilter()
+            groupFilter.addTarget(brightnessFilter)
+        
+        // 设置滤镜组链
+        bilateralFilter.addTarget(brightnessFilter)
+        groupFilter.initialFilters = [bilateralFilter]
+        groupFilter.terminalFilter = brightnessFilter
+        
+        
+        camera.addTarget(groupFilter)
+        
+        groupFilter.addTarget(imageView)
+        
+        camera.startCapture()
+        
+        
+        
+        // 哈哈镜效果
+        let stretchDistortionFilter = GPUImageStretchDistortionFilter()
+        // 亮度
+        //let brightnessFilter = GPUImageBrightnessFilter()
+        // 伽马线滤镜
+        let gammaFilter = GPUImageGammaFilter()
+        
+        // 边缘检测
+        let xYDerivativeFilter = GPUImageXYDerivativeFilter()
+        // 怀旧
+        let sepiaFilter = GPUImageSepiaFilter()
+        /// 反色
+        let invertFilter = GPUImageColorInvertFilter()
+        /// 饱和度
+        let saturationFilter = GPUImageSaturationFilter()
+        /// 美颜
+        //let beautyFielter = GPUImageBilateralFilter()
+        
+        filtersArray = [stretchDistortionFilter,brightnessFilter,gammaFilter,xYDerivativeFilter,sepiaFilter,invertFilter,saturationFilter]
+        
+//        xmCamera?.addTarget(stretchDistortionFilter)
+        
+//        stretchDistortionFilter.addTarget(xmGPUImageView)
+        
+        
+        
+//        camera.startCapture()
         
     }
 
@@ -36,13 +103,14 @@ class ViewController: UIViewController {
 
 
 // MARK: - 给相机加滤镜
-extension ViewController {
-    
-    fileprivate func addFilterToCamera() {
-        // 步骤一：初始化相机，第一个参数表示相册的尺寸，第二个参数表示前后摄像头
-//        GPUImageStillCamera
-    }
-}
+//extension ViewController {
+//    
+//    fileprivate func addFilterToCamera() {
+//        // 步骤一：初始化相机，第一个参数表示相册的尺寸，第二个参数表示前后摄像头
+//        let camera = GPUImageStillCamera()
+//        
+//    }
+//}
 
 
 
