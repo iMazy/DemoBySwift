23677a9123ccb3e899b62a7b01acb8c37661c97a
Mazy <mazongyong@163.com>
2017-04-27 11:26:32 +0800

    feat: 通过pod安装第三方Realm

--- RealmSwiftDemo/RealmSwiftDemo/ViewController.swift
+++ RealmSwiftDemo/RealmSwiftDemo/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+    
+        
+    
+    @IBAction func writeJsonToRealm() {
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func readJsonFromRealm() {
     }
-
-
 }
 
