06528839b640a055e371cb477f2d451a624d5490
Mazy <mazongyong@163.com>
2017-04-27 16:16:05 +0800

    feat: 初始化UICollectionView

--- VerticalScrollingLabel/VerticalScrollingLabel/ViewController.swift
+++ VerticalScrollingLabel/VerticalScrollingLabel/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
-
+let cellID = "CELL"
 class ViewController: UIViewController {
 
+    let flowLayout = UICollectionViewFlowLayout()
+    var collectionView: UICollectionView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        flowLayout.itemSize = CGSize(width: view.bounds.width, height: 50)
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 100, width: view.bounds.width, height: 50), collectionViewLayout: flowLayout)
+        collectionView?.dataSource = self
+        view.addSubview(collectionView!)
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        
+        collectionView?.isPagingEnabled = true
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
 }
 
