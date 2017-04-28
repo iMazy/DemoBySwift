1246bfd7db0c8291dfebdc393fef7e77bd6a284d
Mazy <mazongyong@163.com>
2017-04-28 15:54:22 +0800

    faet: 获取头像大小

--- FaceDetectionByCoreImage/FaceDetectionByCoreImage/ViewController.swift
+++ FaceDetectionByCoreImage/FaceDetectionByCoreImage/ViewController.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var personPicView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        detect()
+        
     }
+    
+    func detect() {
+        
+        guard let personciImage = CIImage(image: personPicView.image!) else {
+            return
+        }
+        
+        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+        let faces = faceDetector?.features(in: personciImage)
+        
+        // issue
+        let ciImageSize = personciImage.extent.size
+        var transform = CGAffineTransform(scaleX: 1, y: -1)
+        transform = transform.translatedBy(x: 0, y: -ciImageSize.height)
+        
+        
+        for face in faces as! [CIFaceFeature] {
+            
+//            var faceViewBounds = CGRect()
+            
+            
+            let faceBox = UIView(frame: face.bounds)
+            faceBox.layer.borderWidth = 1
+            faceBox.layer.borderColor = UIColor.red.cgColor
+            faceBox.backgroundColor = .clear
+            personPicView.addSubview(faceBox)
+            
+            if face.hasLeftEyePosition {
+                print("Left eye bounds are \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Right eye bounds are \(face.hasRightEyePosition)")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            }
+        }
+        
     }
 
 
