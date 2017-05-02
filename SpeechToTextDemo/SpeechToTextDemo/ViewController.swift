0e2cea89d18983b561db349550114a5a5ab528a6
Mazy <mazongyong@163.com>
2017-05-02 09:54:37 +0800

    feat: 识别语言识别是否打开

--- SpeechToTextDemo/SpeechToTextDemo/ViewController.swift
+++ SpeechToTextDemo/SpeechToTextDemo/ViewController.swift
@@ -7,20 +7,54 @@
 //
 
 import UIKit
+import Speech //1
 
 class ViewController: UIViewController {
 
+    /// textView 展示语音文字
     @IBOutlet weak var textView: UITextView!
+    /// 控制语音动作按钮
+    @IBOutlet weak var speechButton: UIButton!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        speechButton.isEnabled = false
         
+        speechRecognizer?.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { (authState) in
+            var isButtonEnabled = false
+            switch authState {
+            case .authorized:
+                isButtonEnabled = true
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+            }
+            
+            OperationQueue.main.addOperation {
+                self.speechButton.isEnabled = isButtonEnabled
+            }
+        }
         
     }
     
     @IBAction func speechAction(_ sender: UIButton) {
+        
     }
+}
 
-
+extension ViewController: SFSpeechRecognizerDelegate {
+    
 }
 
