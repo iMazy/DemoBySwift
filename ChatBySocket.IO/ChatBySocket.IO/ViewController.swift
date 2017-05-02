147bf9f5149c0b81e9b4aa2f1a4c2650520c8092
Mazy <mazongyong@163.com>
2017-05-02 15:11:03 +0800

    feat: 完成用户的展示

--- ChatBySocket.IO/ChatBySocket.IO/ViewController.swift
+++ ChatBySocket.IO/ChatBySocket.IO/ViewController.swift
@@ -8,18 +8,87 @@
 
 import UIKit
 
+
+let cellReuseID = "chatListCell"
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var nickname: String?
+    var users: [[String: AnyObject]] = [[String: AnyObject]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.isHidden = true
+        tableView.tableFooterView = UIView()
+        
+        if nickname == nil {
+            askForNickName()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func askForNickName() {
+        let alertVC = UIAlertController(title: "SocketChat", message: "please enter a nickname", preferredStyle: .alert)
+        alertVC.addTextField { (field: UITextField) in
+            
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            let textField = alertVC.textFields?.first
+            if textField?.text?.characters.count == 0 {
+                self.askForNickName()
+            } else {
+                self.nickname = textField?.text
+                SocketIOManager.shareInstance.connectToServerWithNickname(nickname: self.nickname!, completionHandler: { (userList) in
+                    OperationQueue.main.addOperation {
+                        if userList != nil {
+                            self.users = userList!
+                            self.tableView.reloadData()
+                            self.tableView.isHidden = false
+                        }
+                    }
+                })
+            }
+        }
+        alertVC.addAction(okAction)
+        present(alertVC, animated: true, completion: nil)
+        
     }
+    
+    @IBAction func joinChat(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func exitAction(_ sender: UIBarButtonItem) {
+        
+    }
+    
 
+}
 
+extension ViewController: UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellReuseID)
+        if cell == nil {
+            cell = UITableViewCell(style: .value1, reuseIdentifier: cellReuseID)
+        }
+        cell?.textLabel?.text = users[indexPath.row]["nickname"] as? String
+        cell?.detailTextLabel?.text = (users[indexPath.row]["isConnected"] as! Bool) ? "Online" : "Offline"
+        cell?.detailTextLabel?.textColor = (users[indexPath.row]["isConnected"] as! Bool) ? .green : .red
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
