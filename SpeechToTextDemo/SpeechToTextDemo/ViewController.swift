933a846cddae603eceebe548d65f4d86a5969972
Mazy <mazongyong@163.com>
2017-05-02 09:20:42 +0800

    feat: 初始化界面

--- SpeechToTextDemo/SpeechToTextDemo/ViewController.swift
+++ SpeechToTextDemo/SpeechToTextDemo/ViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func speechAction(_ sender: UIButton) {
     }
 
 
