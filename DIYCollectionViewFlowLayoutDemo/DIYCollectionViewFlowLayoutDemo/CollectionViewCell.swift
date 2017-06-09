0f7ff2d501cfc1a9b71ed3ebb384693a30a4d6d5
Mazy <mazy_ios@163.com>
2017-06-09 14:51:08 +0800

    feat: 初始化界面

---
+++ DIYCollectionViewFlowLayoutDemo/DIYCollectionViewFlowLayoutDemo/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  DIYCollectionViewFlowLayoutDemo
+//
+//  Created by Mazy on 2017/6/9.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ DIYCollectionViewFlowLayoutDemo/DIYCollectionViewFlowLayoutDemo/CollectionViewFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewFlowLayout.swift
+//  DIYCollectionViewFlowLayoutDemo
+//
+//  Created by Mazy on 2017/6/9.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewFlowLayout: UICollectionViewFlowLayout {
+
+}
