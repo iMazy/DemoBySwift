691915e6594b127783f374b46fdbcc37f4cb7da2
Mazy <mazongyong@163.com>
2017-06-18 10:23:55 +0800

    feat: 通过Touch ID 完成基本授权

--- AuthenticateByTouchID/AuthenticateByTouchID/ViewController.swift
+++ AuthenticateByTouchID/AuthenticateByTouchID/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+// 1
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
@@ -15,9 +17,59 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginAction(_ sender: Any) {
+        
+        authenticateByTouchID()
+    }
+    
+    // 2
+    func authenticateByTouchID() {
+        
+        let authContext = LAContext()
+        let authReason = "Please use touch ID to sign in"
+        var authError: NSError?
+        
+        if authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason:authReason, reply: { (success, error) in
+                if success {
+                    print("authenticate successfully")
+                    // want to deal with UI code here
+                    DispatchQueue.main.async {
+                        // 刷新UI
+                    }
+                } else {
+                    if let error = error {
+                        self.reportTouchIDError(error: error as NSError)
+                    }
+                }
+            })
+        } else {
+            // we have an error
+            print(authError?.localizedDescription ?? "error")
+            // show the normal sign up screen
+            
+        }
+    }
+    
+    func reportTouchIDError(error: NSError) {
+        switch error.code {
+        case LAError.authenticationFailed.rawValue:
+            print("authentication Failed")
+        case LAError.passcodeNotSet.rawValue:
+            print("passcode Not Set")
+        case LAError.systemCancel.rawValue:
+            print("system Cancel")
+        case LAError.userCancel.rawValue:
+            print("user cancel")
+        case LAError.touchIDNotEnrolled.rawValue:
+            print("touch ID not enrolled")
+        case LAError.touchIDNotAvailable.rawValue:
+            print("touch ID Not Available")
+        case LAError.userFallback.rawValue:
+            print("user Fall back")
+        default:
+            print(error.localizedDescription)
+        }
     }
 
 
