a366ee9cf4598bf7317bea7012d84db9b772dd34
Mazy <mazy_ios@163.com>
2017-07-25 10:13:53 +0800

    feat: 搭建项目结构，创建基本类

---
+++ NewsFrameworkDemo/NewsFrameworkDemo/NewsFramework/ContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerView.swift
+//  NewsFrameworkDemo
+//
+//  Created by Mazy on 2017/7/25.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NewsFrameworkDemo/NewsFrameworkDemo/NewsFramework/MainContentView.swift
@@ -0,0 +1,14 @@
+//
+//  MainContentView.swift
+//  NewsFrameworkDemo
+//
+//  Created by Mazy on 2017/7/25.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class MainContentView: UIView {
+
+
+}
