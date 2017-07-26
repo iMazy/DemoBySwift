cc8a4cff45bb6a1b732ca3758a61b741c30a35eb
Mazy <mazy_ios@163.com>
2017-07-26 16:48:15 +0800

    meige

--- ChatBySocket.IO/Pods/Target Support Files/Pods-ChatBySocket.IO/Pods-ChatBySocket.IO-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChatBySocket_IO : NSObject
-@end
-@implementation PodsDummy_Pods_ChatBySocket_IO
-@end
--- NewsFrameworkDemo/NewsFrameworkDemo/ViewController.swift
+++ NewsFrameworkDemo/NewsFrameworkDemo/ViewController.swift
@@ -13,22 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let titleArray: [String] = ["金土星","木","水红","火大时代","土收到","木","水红","火大时代","土收到"]
-        let titleArray: [String] = ["金土星","土收到","水红星"]
-        
+        let titleArray: [String] = ["推荐","热点","北京","视频","社会","图片","娱乐","科技","汽车"]
         var childVC: [UIViewController] = [UIViewController]()
         
         for _ in 0..<titleArray.count {
             let vc = UIViewController()
-            vc.view.backgroundColor = UIColor.white
+            vc.view.backgroundColor = UIColor.randomColor().withAlphaComponent(0.3)
             childVC.append(vc)
         }
         
         let property: TitleViewProperty = TitleViewProperty()
         
-//        property.isScrollEnable = true
-//        property.contentOffsetAnimated = false
-        property.shadowOffsetHeight = 2
+        property.isScrollEnable = true
+        property.contentOffsetAnimated = false
+        
+//        property.shadowOffsetHeight = 2
         
         let containerView = ContainerView(frame: CGRect(x: 0, y: 64, width: view.bounds.width, height: view.bounds.height-64), titles: titleArray, childVC: childVC, parentVC: self, titleProperty: property)
         
