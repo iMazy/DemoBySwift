41e3a8943469aca2bebf8f2aec5372f9b153ae39
Mazy <mazy_ios@163.com>
2017-08-28 17:02:02 +0800

    feat: 布局主页面导航栏

--- Solive/Solive/Home/Controller/HomeViewController.swift
+++ Solive/Solive/Home/Controller/HomeViewController.swift
@@ -12,8 +12,29 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupNavigationBar()
     }
+}
 
+
+// MARK: -
+extension HomeViewController {
+    
+    func setupNavigationBar() {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "主播昵称/房间号/链接"
+        searchBar.barStyle = .black
+        searchBar.isTranslucent = true
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "ranking-p")?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(rightItemAction))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "search_btn_follow"), style: .plain, target: self, action: #selector(rightItemAction))
+    }
+    
+    @objc private func rightItemAction() {
+        print("rightItemAction")
+    }
 }
