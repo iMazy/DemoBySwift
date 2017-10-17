4e4d68cfc662400f364d12cf623615c46d15c52c
Mazy <mazy_ios@163.com>
2017-10-17 14:21:15 +0800

    feat： 优化代码

--- DrawAndAnimatedPolygon/DrawAndAnimatedPolygon/PolygonView.swift
+++ DrawAndAnimatedPolygon/DrawAndAnimatedPolygon/PolygonView.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class PolygonView: UIView {
 
-    var values: [CGFloat] = []
+    var values: [CGFloat]? {
+        didSet {
+            cofigDataSource()
+        }
+    }
     // self.radius 为半径
     var radius: CGFloat = 0
     // self.sideNum为总共多少个边
     var sideNum: Int = 6
     // 层级
     var valueRankNum: Int = 3
+    var animationDuration: TimeInterval = 0
+    var lineColor: UIColor?
+    var valueLineColor: UIColor?
     
     fileprivate var centerX: CGFloat = 0
     fileprivate var centerY: CGFloat = 0
@@ -32,17 +39,20 @@ class PolygonView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
- 
-        radius = bounds.width * 0.5
+    }
+    
+    func prepareData() {
         
-        
-        sideNum = values.count
+        sideNum = values?.count ?? 6
+        // 中心的点
         centerX = bounds.size.width/2
         centerY = bounds.size.height/2
         
-        cofigDataSource()
-        
-        
+        radius = radius == 0 ? bounds.size.width * 0.5 : radius
+        lineColor = lineColor == nil ? .white : lineColor
+        valueLineColor = valueLineColor == nil ? .red : valueLineColor
+        valueRankNum = valueRankNum == 0 ? 3 : valueRankNum
+        animationDuration = animationDuration == 0 ? 1.35 : animationDuration
     }
     
     override func layoutSubviews() {
@@ -68,19 +78,20 @@ class PolygonView: UIView {
         addStrokeEndAnimationToLayer(layer: shapeLayer)
     }
     
-    func drawLineFromCenter() {
+    // 绘制线条
+    fileprivate func drawLineFromCenter() {
         let points = self.cornerPointArrs.last!
         for i in 0..<points.count {
             self.bezierPath.move(to: CGPoint(x: centerX, y: centerY))
             let point = points[i].cgPointValue
-            print(point)
             self.bezierPath.addLine(to: point)
             self.bezierPath.lineWidth = 1
         }
-        self.shapeLayer.strokeColor = UIColor.white.cgColor
+        self.shapeLayer.strokeColor = lineColor?.cgColor
     }
     
-    func drawValueSide() {
+    // 绘制多边形
+    fileprivate func drawValueSide() {
         if valuePoints.count == 0 { return }
         let firstPoint = valuePoints[0].cgPointValue
         valuePath.move(to: firstPoint)
@@ -91,43 +102,41 @@ class PolygonView: UIView {
             valuePath.lineWidth = 1
         }
         valuePath.addLine(to: firstPoint)
-        valueLayer.fillColor = UIColor.orange.cgColor
-        valueLayer.strokeColor = UIColor.blue.cgColor
+        valueLayer.fillColor = valueLineColor?.cgColor.copy(alpha: 0.5)
+        valueLayer.strokeColor = valueLineColor?.cgColor
         valueLayer.path = valuePath.cgPath
     }
     
+    // 设置数据
     private func cofigDataSource() {
         
-        
+        prepareData()
         
         makePoint()
     }
     
-    func makePoint() {
+    
+    /// 获取图形的各个点
+    fileprivate func makePoint() {
         var tempValuePoints: [NSValue] = [NSValue]()
         var tempCornerPointArrs: [[NSValue]] = [[NSValue]]()
         
-        // 计算各个点的坐标
-        /// 1、中心的点
-
-        
-        /// 2、边上的角度
-        // 1弧度=180/π度
-        // 1度=π/180弧度
+        // 计算多边形各个点的坐标
         // i为逆时针第i个点
         for i in 0..<sideNum {
-            if values.count > i {
-                let valueRadius:  CGFloat = values[i] * radius
+            if values!.count > i {
+                // 多边形每个边的长度
+                let valueRadius:  CGFloat = values![i] * radius
+                // 每个边长度所在的点
                 let valuePoint = CGPoint(x: centerX-getAngleCos(90.0-360.0/CGFloat(sideNum)*CGFloat(i))*valueRadius, y: centerY-getAngleSin(90.0-360.0/CGFloat(sideNum)*CGFloat(i))*valueRadius)
                 tempValuePoints.append(NSValue(cgPoint: valuePoint))
             }
         }
         
-        
+        // 每个等级所占的宽度
         let rankValue: CGFloat = radius/CGFloat(valueRankNum)
         for j in 0..<valueRankNum {
             var tempCornerPoints:[NSValue] = [NSValue]()
-            
             for i in 0..<sideNum {
                 let rank = j + 1
                 let cornerPoint = CGPoint(x:
@@ -143,18 +152,20 @@ class PolygonView: UIView {
     }
     
     
-    func drawSide() {
+    // 开始画线
+    fileprivate func drawSide() {
         for p in cornerPointArrs {
             drawLineWithPoints(points: p, color: .yellow)
         }
     }
-    
-    func drawLineWithPoints(points: [NSValue], color: UIColor) {
+    // 通过点数组连线
+    fileprivate func drawLineWithPoints(points: [NSValue], color: UIColor) {
         if points.count == 0 { return }
+        // 第一个点（也是最后一个点）
         let firstPoint = points[0].cgPointValue
         
         bezierPath.move(to: firstPoint)
-        
+        // 循环连线
         for i in 0..<points.count {
             let point = points[i].cgPointValue
             bezierPath.addLine(to: point)
@@ -162,7 +173,7 @@ class PolygonView: UIView {
         }
         bezierPath.addLine(to: firstPoint)
     }
-    
+    /*
     func getPointWithRadius(radius: CGFloat) -> [NSValue] {
         var inCornerPoints: [NSValue] = [NSValue]()
         for i in 0..<sideNum {
@@ -172,8 +183,10 @@ class PolygonView: UIView {
         }
         return inCornerPoints
     }
+     */
     
-    func addStrokeEndAnimationToLayer(layer: CAShapeLayer) {
+    /// 添加动画
+    fileprivate func addStrokeEndAnimationToLayer(layer: CAShapeLayer) {
         let animation = CABasicAnimation(keyPath: "strokeEnd")
         animation.fromValue = 0
         animation.toValue = 1
@@ -181,10 +194,12 @@ class PolygonView: UIView {
         layer.add(animation, forKey: "stokeEndAnimation")
     }
     
+    // 通过角度获取 cos 值
     func getAngleCos(_ angle: CGFloat) -> CGFloat {
         return CGFloat(cos(Double.pi/180.0 * Double(angle)))
     }
     
+    // 通过角度获取 sin 值
     func getAngleSin(_ angle: CGFloat) -> CGFloat {
         return CGFloat(sin(Double.pi/180.0 * Double(angle)))
     }
@@ -192,13 +207,5 @@ class PolygonView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
--- DrawAndAnimatedPolygon/DrawAndAnimatedPolygon/ViewController.swift
+++ DrawAndAnimatedPolygon/DrawAndAnimatedPolygon/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         view.backgroundColor = .black
         
         let w: CGFloat = UIScreen.main.bounds.width
-        polygonView = PolygonView(frame: CGRect(x: (w-200)/2, y: (w-200)/2, width: 200, height: 200))
+        polygonView = PolygonView(frame: CGRect(x: (w-300)/2, y: (w-200)/2, width: 300, height: 300))
         polygonView?.values = [0.3, 0.7, 0.4, 0.6, 0.7, 0.4]
         view.addSubview(polygonView!)
         polygonView?.show()
@@ -27,6 +27,9 @@ class ViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        _ = polygonView?.subviews.map({ $0.removeFromSuperview() })
+        
+        polygonView?.didMoveToSuperview()
         
     }
 
