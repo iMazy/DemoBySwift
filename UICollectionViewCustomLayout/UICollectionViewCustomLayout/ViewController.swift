ea5e4a448d73ec1f726419d039d1e36255bb915e
Mazy <mazongyong@163.com>
2018-12-26 20:21:39 +0800

    init UICollectionViewCustomLayout project

---
+++ UICollectionViewCustomLayout/UICollectionViewCustomLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewCustomLayout
+//
+//  Created by Mazy on 2018/12/26.
+//  Copyright © 2018 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
